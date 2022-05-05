e0f44d52cfaf5d8cd97cccfeab14c51baa014bcf
Kamil Boryczka <leoctaneee@gmail.com>
2022-05-05 13:33:45 +0200

    init

---
+++ android/app/src/main/kotlin/com/example/my_transformation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_transformation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyTransformationApp());
+}
+
+class MyTransformationApp extends StatelessWidget {
+  const MyTransformationApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
